91d865f3e1e6edd6a53513aa66e3b9e2e673cf2d
L4K0NL98JB <juhanasaarinen@tunturipaju.local>
2016-03-09 15:57:44 +0200

    Service wrapper for mobiilitiedekerho backend.

---
+++ deploy-scripts/mobiilitiedekerho.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+ case $1 in
+    start)
+       nohup java -jar /home/mobiili/app/jar/mobiilitiedekerho-backend-1.0-SNAPSHOT-jar-with-dependencies.jar > /home/mobiili/app/log/stdout.out 2>&1 &
+       echo $! > /home/mobiili/app/log/mobiilitiedekerho.pid
+       ;;
+     stop)
+       kill $(cat /home/mobiili/app/log/mobiilitiedekerho.pid);
+       rm /home/mobiili/app/log/mobiilitiedekerho.pid
+       ;;
+     *)
+       echo "usage: mobiilitiedekerho {start|stop}" ;;
+ esac
+ exit 0
+ 
